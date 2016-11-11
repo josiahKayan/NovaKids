59ace4ea7ca37d271accfda0e3c45d9dbc6ef6a6
Josias Kayan <jkayanlima@gmail.com>
2016-11-10 23:40:51 -0400

    Criação do Domain

---
+++ NovaKidsControl.Domain/Models/Crianca.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovaKidsControl.Domain.Models
+{
+    public class Crianca : UserKids
+    {
+        public new int Id { get; set; }
+
+        public Crianca() { }
+
+        public Crianca(int id,string name, string data, string horaEntrada, string horarioKids, string matricula )
+        {
+            Id = id;
+            this.Name = name;
+            this.Data = data;
+            this.HoraEntrada = horaEntrada;
+            this.HorarioKids = horarioKids;
+            this.Matricula = matricula;
+        }
+
+
+
+    }
+}
---
+++ NovaKidsControl.Domain/Models/Professor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovaKidsControl.Domain.Models
+{
+    public class Professor : UserKids
+    {
+        public new int Id { get; set; }
+
+        public Professor() { }
+
+        public Professor(int id, string name, string data, string horaEntrada, string horarioKids, string matricula)
+        {
+            Id = id;
+            this.Name = name;
+            this.Data = data;
+            this.HoraEntrada = horaEntrada;
+            this.HorarioKids = horarioKids;
+            this.Matricula = matricula;
+        }
+    }
+}
